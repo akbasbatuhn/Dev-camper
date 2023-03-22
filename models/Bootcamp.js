e1e70609139af14e8f978fa5190d0e2d8409f513
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-03-22 21:18:12 +0300

    Bootcamp & courses ownership

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,10 +99,17 @@ const BootcampSchema = new mongoose.Schema(
             type: Date,
             default: Date.now,
         },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: true,
+        },
     },
     {
         toJSON: { virtuals: true },
         toObject: { virtuals: true },
+        // Prevent duplicate id with _id
+        id: false,
     }
 );
 
