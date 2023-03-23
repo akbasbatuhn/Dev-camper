3afd0a632f6cd63100f65b75b39aba6323433d5b
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-03-23 17:02:16 +0300

    unnecessary controller method

--- controllers/auth.js
+++ controllers/auth.js
@@ -95,15 +95,6 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
-// @desc      Get current logged in user
-// @route     GET /api/v1/auth/me
-// @access    Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({ success: true, data: user });
-});
-
 // @desc      Forgot password
 // @route     POST /api/v1/auth/forgotpassword
 // @access    Public
