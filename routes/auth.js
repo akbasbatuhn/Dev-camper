3eab39c4fb589c6bb04b512148f993b0baee7f7b
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-03-24 12:58:06 +0300

    Added logout to clear cookie

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
     resetPassword,
     updateDetails,
     updatePassword,
+    logout,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const { protectRoutes } = require("../middleware/auth");
 
 router.post("/register", registerUser);
 router.post("/login", login);
+router.get("/logout", logout);
 router.get("/me", protectRoutes, getMe);
 router.put("/updatedetails", protectRoutes, updateDetails);
 router.put("/updatepassword", protectRoutes, updatePassword);
